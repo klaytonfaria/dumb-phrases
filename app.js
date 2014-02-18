4ecbe52040a6178280aca61fbadb3fbbb0b77878
Klayton Faria <klayton.souza@gmail.com>
2014-02-18 19:08:27 -0300

    App Refactoring - Clean routes

--- app.js
+++ app.js
@@ -1,61 +1,34 @@
+// Module dependencies
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes/routes');
-var http = require('http');
-var path = require('path');
-var cons = require('consolidate');
-var DumbPhrasesProvider = require('./providers/provider').DumbPhrasesProvider;
-var app = express();
+var express = require('express'),
+  http = require('http'),
+  routes = require('./routes/routes'),
+  path = require('path'),
+  cons = require('consolidate'),
+  app = express(),
+  appPath = {
+    index: "/",
+    phrases: {
+      all: "/phrases/all",
+      add: "/phrases/add",
+    }
+  };
 
 // all environments
 app.configure(function() {
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', path.join(__dirname, 'views'));
-	app.use(express.static(path.join(__dirname, 'public')));
 	app.engine('hbs', cons.handlebars);
 	app.set('view engine', 'hbs');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-var dumbPhrasesProvider = new DumbPhrasesProvider('localhost', 27017, {safe:false});
-
 // Routes
-app.get('/', routes.index);
-//app.get('/phrases/all', routes.all);
-app.get('/phrases/new', routes.new_get);
-//app.post('/phrases/new', routes.new_post);
+app.get(appPath.index, routes.index);
+app.get(appPath.phrases.all, routes.all);
+app.get(appPath.phrases.add, routes.new_get);
+app.post(appPath.phrases.add, routes.new_post);
 
-app.get('/phrases/all', function(req, res) {	
-  dumbPhrasesProvider.findAll(function(error, phrases){
-      res.render('all', {
-    	title: "Dumb Phrases",
-    	phrases : phrases
-  	});
-  });
-});
-
-//save new phrase
-app.post('/phrases/new', function(req, res){
-    dumbPhrasesProvider.save({      
-        title: req.param('title'),
-    	author: {
-    		name: req.param('name'), 
-    		age: req.param('mental-age')
-    	},
-    	phrase : req.param('phrase')
-    }, function(error, docs) {
-        res.redirect('/phrases/all')
-    });
-});
-
-
+// Create and listen server application
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
