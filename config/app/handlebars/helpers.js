bcb9442dbbb30295abea811531ee3aa1bde43888
Klayton Faria <klayton.souza@gmail.com>
2014-08-08 16:26:54 -0300

    Starts new helper to load projection

--- config/app/handlebars/helpers.js
+++ config/app/handlebars/helpers.js
@@ -1,46 +1,37 @@
 var settings = require('../settings'),
+    Client = require('node-rest-client').Client,
+    client = new Client(),
     request = require('request'),
     Handlebars = require('hbs'),
     Step = require('step');
 
-function getData(url, callback) {
-  // Loading projection
-  request({url: url, json: true}, function (error, response, data) {
-    if (!error && response.statusCode == 200) {
-      callback(data);
+
+Handlebars.registerHelper('projection', function(context, options) {
+
+  return context.posts.title;
+});
+
+Handlebars.registerHelper('context', function(context, options) {
+  var content = {"posts": {
+    "_id":"53c5262692c760e0e7000003",
+    "title":"Teste",
+    "author": {
+      "name":"Klayton Faria",
+      "age":"27"},
+      "phrase":"Podemos jÃ¡ vislumbrar o modo pelo qual a estrutura atual da organizaÃ§Ã£o prepara-nos para enfrentar situaÃ§Ãµes atÃ­picas decorrentes do remanejamento dos quadros funcionais."
     }
-  });
-}
+  };
 
-function getContext(url) {
-  returngetData(url, function(data){
-    data = JSON.stringify(data);
-    return data;
-  });
-}
-
-Handlebars.registerHelper('posts', function(context, options) {
-  var content = {posts:[{name:"klayton"}]};
   // Building url
-  var url = settings.constants.SERVICES_PATH + context;
+  /*var url = settings.constants.SERVICES_PATH + context;
   for(key in options.hash) {
     if(options.hash.hasOwnProperty("id") && key === "id") {
       url = url + "/" + options.hash["id"];
-      //content = getContext(url);
+      client.get(url, function(data, response) {
+        content = data;
+      });
     }
-  }
+  }*/
 
-    console.log(content);
-    return JSON.stringify(content);
-});
-
-
-Handlebars.registerHelper('list', function(items, options) {
-  var out = "<ul>";
-
-  for(var i=0, l=items.length; i<l; i++) {
-    out = out + "<li>" + options.fn(items[i]) + "</li>";
-  }
-
-  return out + "</ul>";
+  return content;
 });
