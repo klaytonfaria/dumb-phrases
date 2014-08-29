8292932b739e6b29e65ea554d2405348e3a777ea
Klayton Faria <klayton.souza@gmail.com>
2014-08-29 19:54:58 -0300

    Add helper to load context

--- app/extend/helpers.js
+++ app/extend/helpers.js
@@ -4,33 +4,37 @@ var appPath = process.cwd(),
     client = new Client(),
     Handlebars = require("hbs");
 
-
 Handlebars.registerHelper("projection", function(context, options) {
 
   return context.posts.title;
 });
 
 Handlebars.registerHelper("context", function(context, options) {
-  var content = {"posts": {
-    "_id":"53c5262692c760e0e7000003",
-    "title":"Teste",
-    "author": {
-      "name":"Klayton Faria",
-      "age":"27"},
-      "phrase":"Podemos jÃ¡ vislumbrar o modo pelo qual a estrutura atual da organizaÃ§Ã£o prepara-nos para enfrentar situaÃ§Ãµes atÃ­picas decorrentes do remanejamento dos quadros funcionais."
-    }
-  };
 
   // Building url
-  /*var url = settings.constants.SERVICES_PATH + context;
+  var url = settings.constants.SERVICES_PATH + context;
   for(key in options.hash) {
     if(options.hash.hasOwnProperty("id") && key === "id") {
       url = url + "/" + options.hash["id"];
-      client.get(url, function(data, response) {
-        content = data;
-      });
     }
-  }*/
+  }
 
-  return content;
+  var responseData = {
+        "posts": {
+        "_id":"53c5262692c760e0e7000003",
+        "title":"Não funcionou",
+        "author": {
+          "name":"Klayton Faria",
+          "age":"27"},
+          "phrase":"Conteúdo de remanejamento dos quadros funcionais."
+        }
+      };
+
+  client.get(url, function(data) {
+      responseData = data;
+    }
+  );
+
+// TODO : Make urlrequest function synchronous
+  return options.fn(responseData);
 });
