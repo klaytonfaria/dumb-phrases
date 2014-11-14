699f5a13c6f1a396544c494391208cf4d5c61bba
Klayton Faria <klayton.souza@gmail.com>
2014-11-14 14:21:31 -0200

    Add client

---
+++ client/app/routes/router.js
@@ -0,0 +1,15 @@
+define(function (App) {
+  App.Router.map(function() {
+    this.resource('users', {path: '/usuarios'},function() {
+        this.route('teste');
+      }
+    );
+  });
+
+  App.UsersRoute = Ember.Route.extend({
+    model: function() {
+      // jQuery AJAX - returns a PROMISE!
+      return {}//Ember.$.getJSON('http://demoember.ved/resources/beers.json');
+    }
+  })
+});
